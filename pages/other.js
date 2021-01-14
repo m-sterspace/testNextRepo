62aa854b600a56d8a9be779325bd6861a024e538
Jason Masters <JMasters@dialogdesign.ca>
2021-01-14 11:44:31 -0500

    added second page

---
+++ pages/other.js
@@ -0,0 +1,15 @@
+import SmallCard from '../components/SmallCard';
+import { projectIcons } from '../components/Icons';
+
+import { projects } from '../utils/projectsData';
+
+const Other = () => (
+  <div className="home">
+    <h1>2nd Page!</h1>
+    <div className="card-grid">
+
+    </div>
+  </div>
+);
+
+export default Other;
